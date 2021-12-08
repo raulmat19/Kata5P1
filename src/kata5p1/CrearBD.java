ebb08fa13b06818e6103133fc09e856c8d783601
raulmat19 <91469476+raulmat19@users.noreply.github.com>
2021-12-08 18:53:41 +0000

    Kata5P1

---
+++ src/kata5p1/CrearBD.java
@@ -0,0 +1,45 @@
+package kata5p1;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CrearBD {
+    
+    public static void createNewDatabase(String fileName){
+        String url = "jdbc:sqlite:" + fileName;
+        
+        try (Connection conn = DriverManager.getConnection(url)){
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("El driver es " + meta.getDriverName());
+                System.out.println("Se ha creado una nueva BD.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void createNewTable() {
+        // Cadena de conexión SQLite
+        String url = "jdbc:sqlite:mail.db";
+        
+        // Instrucción SQL para crear nueva tabla
+        String sql = "CREATE TABLE IF NOT EXISTS direcc_email (\n"
+                   + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                   + " direccion text NOT NULL);";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement()) {
+            
+            // Se crea la nueva tabla
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
